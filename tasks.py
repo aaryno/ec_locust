b54daf935cabc65f34030ca51af30f6ad1348015
Sasha Hart <harts@boundlessgeo.com>
2017-07-25 12:01:40 -0500

    split out wms code from task code per se, etc.

--- tasks.py
+++ tasks.py
@@ -2,13 +2,17 @@
 """
 import random
 from locust import HttpLocust, TaskSet, task
+from wms import WMSBehavior
 
 
-class Behavior(TaskSet):
+class Behavior(WMSBehavior):
     """Specify the set of tasks simulated users will perform.
     """
 
     # Define paths to use, etc.
+
+    exception_format = "application/json"
+    output_format = "application/json"
 
     def __init__(self, parent):
         super(Behavior, self).__init__(parent)
@@ -16,13 +20,14 @@ class Behavior(TaskSet):
     def login(self):
         """
         """
-        response = self.client.post(
-            "/something", json={
-                "username": "basic@boundlessgeo.com",
-                "password": "test_1234",
-            },
-        )
-        response.json()
+        pass
+        # response = self.client.post(
+        #     "/something", json={
+        #         "username": "basic@boundlessgeo.com",
+        #         "password": "test_1234",
+        #     },
+        # )
+        # response.json()
 
     def on_start(self):
         """Startup method automatically called by locust.
@@ -30,8 +35,42 @@ class Behavior(TaskSet):
         self.login()
 
     @task
-    def some_task(self):
-        pass
+    def test_get_capabilities(self):
+        response = self.get_capabilities(path="wms", version="1.3.0")
+
+        # Locust automatically marks fail if it's not a 200
+
+        # This doesn't seem to support JSON output format anyway
+        content_type = response.headers["Content-Type"]
+        if content_type != 'text/xml;charset=UTF-8':
+            response.failure("Unexpected content-type")
+
+        if not response.content:
+            response.failure("Empty response body")
+
+    @task(0)
+    def test_describe_layer(self):
+        version = "1.1.1"
+        # layers
+        response = self.describe_layer(
+            version, self.exception_format, self.output_format,
+        )
+        data = response.json()
+
+    @task(0)
+    def test_get_legend_graphic(self):
+        response = self.get_legend_graphic()
+        data = response.json()
+
+    @task(0)
+    def test_get_feature_info(self):
+        response = self.get_feature_info()
+        data = response.json()
+
+    @task(0)
+    def test_get_map(self):
+        response = self.get_map()
+        data = response.json()
 
 
 class User(HttpLocust):
